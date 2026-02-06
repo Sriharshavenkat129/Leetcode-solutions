32bbb1c1a8ec348d8310d690065cfc19bcf254d7
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-02-06 13:42:28 +0530

    Time: 25 ms (58.63%) | Memory: 86 MB (87.15%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+           Arrays.sort(nums);
+           if(nums.length==1)
+           return 0;
+           if(nums.length==2 && nums[0]*k>=nums[1])
+           return 0;
+           /*
+           int start=0;
+           int end=1;
+           int ans=100000;
+           while(start<=end && end<nums.length){
+           // System.out.println(start+"  "+end);
+            if(nums[start]*k>=nums[end] || nums[start]*k<0){
+                end++;
+            }
+            else{
+                System.out.println(nums[start]*k+" "+nums[end]);
+                System.out.println("break");
+                System.out.println(start+""+end);
+                ans=Math.min(ans,nums.length-end+(start));
+                start++;
+            }
+           }
+            ans=Math.min(ans,nums.length-end+(start));
+            if(start==0 && end==nums.length)
+            return 0;
+    */
+        int ans=1000000000;
+        int right=0;
+           for(int i=0;i<nums.length-1;i++){
+            while(right<nums.length && (long)nums[i]*k>=nums[right]){
+                right++;
+            }
+            ans=Math.min(ans,nums.length-(right-i));
+        }
+           return ans;
+    }
+}
