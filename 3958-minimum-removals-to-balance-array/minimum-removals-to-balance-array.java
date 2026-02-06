849979f327c9ec37aa20d92164f765a8bd6f3fc5
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-02-06 13:49:05 +0530

    Time: 1695 ms (5.62%) | Memory: 80.9 MB (100.00%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -5,13 +5,12 @@ class Solution {
            return 0;
            if(nums.length==2 && nums[0]*k>=nums[1])
            return 0;
-           /*
            int start=0;
            int end=1;
-           int ans=100000;
+           int ans=nums.length;
            while(start<=end && end<nums.length){
            // System.out.println(start+"  "+end);
-            if(nums[start]*k>=nums[end] || nums[start]*k<0){
+            if((long)nums[start]*k>=nums[end]){
                 end++;
             }
             else{
@@ -25,7 +24,7 @@ class Solution {
             ans=Math.min(ans,nums.length-end+(start));
             if(start==0 && end==nums.length)
             return 0;
-    */
+/*
         int ans=1000000000;
         int right=0;
            for(int i=0;i<nums.length-1;i++){
@@ -34,6 +33,8 @@ class Solution {
             }
             ans=Math.min(ans,nums.length-(right-i));
         }
+        */
            return ans;
+           
     }
 }
