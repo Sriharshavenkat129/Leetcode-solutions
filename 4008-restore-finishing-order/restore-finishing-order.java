32be90cb2476edf7d606434df5810ff6c7aa11c5
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-02-09 19:45:44 +0530

    Time: 1 ms (99.91%) | Memory: 47 MB (39.61%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.java
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
-        HashSet<Integer> set=new HashSet<>();
+        /*HashSet<Integer> set=new HashSet<>();
         for(int i:friends)set.add(i);
         int[] ans=new int[friends.length];
         int index=0;
@@ -8,6 +8,16 @@ class Solution {
             if(set.contains(num)){
                 ans[index]=num;
                 index++;
+            }
+        }*/
+        int[] ans=new int[friends.length];
+        int index=0;
+        for(int num:order){
+            for(int i:friends){
+                if(num==i){
+                    ans[index]=num;
+                    index++;
+                }
             }
         }
         return ans;
