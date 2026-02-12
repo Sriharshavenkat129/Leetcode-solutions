28b4d17cfce3ec1bdef7f61c95f5064502c4f237
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-02-12 21:58:16 +0530

    Time: 131 ms (63.86%) | Memory: 44.7 MB (98.13%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestBalanced(String s) {
+        int [] arr=new int[26];
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            Arrays.fill(arr,0);
+            for(int j=i;j<s.length();j++){
+                boolean matched=true;
+                arr[s.charAt(j)-'a']+=1;
+                for(int c:arr){
+                    if(c>0 && c!=arr[s.charAt(j)-'a']){
+                        matched=false;
+                        break;
+                    }
+                }
+                if(matched)
+                ans=Math.max(j-i+1,ans);
+            }
+        }
+        return ans;
+    }
+}
