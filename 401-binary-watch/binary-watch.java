6d104a983757d81f639bf7ecd65c6415a84894a7
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-02-17 20:40:38 +0530

    Time: 6 ms (60.33%) | Memory: 49.6 MB (21.98%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -4,7 +4,9 @@ class Solution {
         List<String> ans = new ArrayList<String>();
         for (int h = 0; h < 12; ++h) {
             for (int m = 0; m < 60; ++m) {
-                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                int hours=Integer.bitCount(h);
+                int mins= Integer.bitCount(m);
+                if ( hours+mins == turnedOn) {
                     ans.add(h + ":" + (m < 10 ? "0" : "") + m);
                 }
             }
