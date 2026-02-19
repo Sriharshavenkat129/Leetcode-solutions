15a0bcbe0ebbff0cc98a14e451021bfe74d44f64
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-02-19 06:56:26 +0530

    Time: 13 ms (5.87%) | Memory: 47.9 MB (7.49%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<groupSizes.length;i++){
+            ArrayList<Integer> list=new ArrayList<>();
+            list.add(i);
+            if(map.containsKey(groupSizes[i])){
+                ArrayList<Integer> l=new ArrayList<>();
+                l=map.get(groupSizes[i]);
+                l.addAll(list);
+                map.put(groupSizes[i],l);
+            }
+            else
+                map.put(groupSizes[i],list);
+        }
+        for(int n:map.keySet()){
+            ArrayList<Integer> l=new ArrayList<>();
+            l=map.get(n);
+            if(l.size()>groupSizes[l.get(0)]){
+                for(int i=0;i<l.size()/groupSizes[l.get(0)];i++){
+                    ArrayList<Integer> newone=new ArrayList<>();
+                    for(int j=i*groupSizes[l.get(0)];j<groupSizes[l.get(0)]+i*groupSizes[l.get(0)];j++){
+                        newone.add(l.get(j));
+                    }
+                    ans.add(newone);
+                }
+            }
+            else{
+                ans.add(l);
+            }
+        }
+        return ans;
+    }
+}
