3a4e582b2b6741c61f2d26326aa41554ccd86782
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-03-03 10:53:21 +0530

    Time: 37 ms (2.45%) | Memory: 45.1 MB (5.29%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int index=0;
+        if(s.length()==0) return true;
+        if(t.length()==0) return false;
+        for(int i=0;i<t.length();i++){
+            if(t.charAt(i)==s.charAt(index))index++;
+            if(index==s.length()) return true;
+            System.out.println(index);
+        }
+        return false;
+    }
+}
