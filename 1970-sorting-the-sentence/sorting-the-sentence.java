fba98aff48a1821fde13d6ff674ab8c9aa5c4156
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-04-12 12:08:57 +0530

    Time: 3 ms (9.57%) | Memory: 42.7 MB (72.56%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words=s.split(" ");
+        String [] arranged=new String[words.length];
+        for(String word:words){
+            arranged[word.charAt(word.length()-1)-'1']=word.substring(0,word.length()-1);
+        }
+        System.out.println(Arrays.toString(arranged));
+        StringBuilder str=new StringBuilder("");
+        for(String word:arranged){
+            str.append(word+" ");
+        }
+        return str.toString().substring(0,str.length()-1);
+    }
+}
