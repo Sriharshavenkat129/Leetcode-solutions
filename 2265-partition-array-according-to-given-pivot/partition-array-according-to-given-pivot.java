d660b516257e3027da101a3f072e7db02c7bcb1e
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-06-08 20:23:19 +0530

    Time: 6 ms (31.24%) | Memory: 160 MB (37.05%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,68 +1,27 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-      /*  for(int i=0;i<nums.length;i++){
-   //         int ele=nums[i];
-            if(nums[i]>pivot){
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[j]<=pivot){
-                        int temp=nums[i];
-                        nums[i]=nums[j];
-                        nums[j]=temp;
-                        break;
-                    }
-                }
-            }
-        }
-        return nums;
-        */
-        /*
-        ArrayList<Integer> list1=new ArrayList<Integer>();
-        ArrayList<Integer> list2=new ArrayList<Integer>();
-        int pivot_count=0;
-        for(int num:nums){
-            if(num<pivot)list1.add(num);
-            else if(num>pivot)list2.add(num);
-            else pivot_count++;
-        }
-        int index=0;
-        for(int num:list1){
-            nums[index]=num;
-            index++;
-        }
-        for(int i=0;i<pivot_count;i++){
-            nums[index]=pivot;
-            index++;
-        }
-        for(int num:list2){
-            nums[index]=num;
-            index++;
-        }
-        return nums;
-        */
         int [] res=new int[nums.length];
-        int left=0,right=nums.length-1;
-        for(int i=0,j=nums.length-1;i<nums.length;i++,j--){
-            if(nums[i]<pivot){
+        int count=0;
+        int left=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==pivot)count++;
+            else if(nums[i]<pivot){
                 res[left]=nums[i];
                 left++;
-            }
-            if(nums[j]>pivot){
-                res[right]=nums[j];
-                right--;
+                nums[i]=Integer.MIN_VALUE;
             }
         }
-        while(left<=right){
+        for(int i=0;i<count;i++){
             res[left]=pivot;
             left++;
         }
-        return res;
-    }
-        static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
+        System.out.println(left);
+        for(int num:nums){
+            if(num>pivot){
+                res[left]=num;
+                left++;
             }
-        }));
+        }
+        return res;
     }
 }
