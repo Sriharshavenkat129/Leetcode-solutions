33904a18a44222e5e1dfe400219312e3f9fa92ac
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-07-19 16:07:50 +0530

    Time: 2 ms (86.89%) | Memory: 44.2 MB (22.16%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[]freqs=new int[26];
+        for(char ch:s.toCharArray()){
+            freqs[ch-'a']++;
+        }
+        int[] used=new int[26];
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+           // System.out.println(stack.toString());
+            while (!stack.isEmpty() &&used[ch-'a']==0 && stack.peek()>ch && freqs[stack.peek()-'a']>0){
+                used[stack.peek()-'a']--;
+                stack.pop();
+            }
+            if(used[ch-'a']<1){
+                stack.push(ch);
+                used[ch-'a']++;
+            }
+            freqs[ch-'a']--;
+        }
+       // System.out.println(stack.toString());
+       StringBuilder ans=new StringBuilder("");
+       while(!stack.isEmpty()){
+        ans.append(stack.pop());
+       }
+        return ans.reverse().toString();
+    }
+}
