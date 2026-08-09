637071a7df1a24df30cd7d3ab390ced4b531c0df
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-08-09 21:27:01 +0530

    Time: 2 ms (97.98%) | Memory: 43.6 MB (52.97%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int n:arr)map.put(n,map.getOrDefault(n,0)+1);
+        HashSet<Integer> set=new HashSet<>();
+        for(int value:map.values()){
+            if(set.contains(value))return false;
+            set.add(value);
+        }
+        return true;
+    }
+}
