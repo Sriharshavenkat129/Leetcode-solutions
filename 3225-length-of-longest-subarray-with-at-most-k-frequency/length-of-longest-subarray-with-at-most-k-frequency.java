2077f4b8ce19566dd84fd78b93cb3af66ed3ec2c
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-08-12 10:04:39 +0530

    Time: 71 ms (36.65%) | Memory: 101.4 MB (19.25%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -7,10 +7,9 @@ class Solution {
             map.put(nums[right],map.getOrDefault(nums[right],0)+1);
             while(map.get(nums[right])>k){
                 map.put(nums[left],map.getOrDefault(nums[left],0)-1);
-                if(map.get(nums[left])==0)map.remove(nums[left]);
                 left++;
             }
-            if(map.get(nums[right])<=k)ans=Math.max(ans,right-left+1);
+            ans=Math.max(ans,right-left+1);
         }
         return ans;
     }
