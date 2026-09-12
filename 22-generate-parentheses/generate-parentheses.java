7ec4049dc902b4c377d518fa036c25baa94ae788
SRI HARSHA VENKAT DUGGIRALA <sriharshavenkat123@gmail.com>
2026-09-12 12:08:28 +0530

    Time: 2 ms (69.53%) | Memory: 44.8 MB (32.37%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,44 +1,17 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans=new ArrayList<>();
-       // helper(1,n,ans);
-       helper(ans,0,0,"",n);
+        generate(ans,0,0,n,"");
         return ans;
     }
-    public void helper(List<String>ans,int left,int right,String s,int n){
-        if(s.length()==2*n){
-            ans.add(s);
-            return;
-        }
+    public void generate(List<String> ans,int left,int right,int n,String str){
+        if(left==n && right==n)
+        ans.add(str);
         if(left<n){
-            helper(ans,left+1,right,s+"(",n);
+            generate(ans,left+1,right,n,str+"(");
         }
-        if(right<left){
-            helper(ans,left,right+1,s+")",n);
+        if(right<left && right<n){
+            generate(ans,left,right+1,n,str+")");
         }
     }
-    /*
-    public void helper(int i,int n,List<String>ans){
-        if(i>n)
-        return;
-        else{
-            List<String> newans=new ArrayList<>();
-            if(i==1){
-                ans.add("()");
-            }
-            else{
-                for(int j=0;j<ans.size();j++){
-                    String s=ans.get(j);
-                    newans.add(s+"()");
-                    newans.add("("+s+")");
-                    if(j>0)
-                    newans.add("()"+s);
-                }
-                ans.clear();
-                ans.addAll(newans);
-            }
-        }
-        helper(i+1,n,ans);
-    }
-    */
 }
