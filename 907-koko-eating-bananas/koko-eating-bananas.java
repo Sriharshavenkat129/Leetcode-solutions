cbb671eb04009417f2c56ba2b68a30ca308adb87
SRI HARSHA VENKAT DUGGIRALA <sriharshavenkat123@gmail.com>
2026-09-15 16:51:12 +0530

    Time: 28 ms (6.80%) | Memory: 47.3 MB (97.75%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left=1,right=piles[0];
+        for(int i:piles){
+            right=Math.max(i,right);
+        }
+        while(left<right){
+            int mid=left+(right-left)/2;
+            int hours=getHours(piles,mid);
+            if(hours<=h)right=mid;
+            else left=mid+1;
+        }
+        return left;
+    }
+    public int getHours(int[] piles,int k){
+        int hours=0;
+        for(int n:piles){
+            hours+=(int)Math.ceil((double)n/k);
+        }
+        System.out.println("---");
+        return hours;
+    }
+}
