a8556415c3b24c9698d14ddb138ca62c9b9b4236
SRI HARSHA VENKAT DUGGIRALA <sriharshavenkat123@gmail.com>
2026-09-22 11:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (91.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -4,16 +4,12 @@ class Solution {
         while(start<=end){
             int mid=start+(end-start)/2;
             if(nums[mid]==target)return mid;
-            else if(nums[start]<=nums[mid]){
-                if(nums[start]<=target && nums[mid]>target){
-                    end=mid-1;
-                }
+            if(nums[start]<=nums[mid]){
+                if(nums[mid]>target && nums[start]<=target)end=mid-1;
                 else start=mid+1;
             }
             else{
-                if(nums[mid]<target && target<=nums[end]){
-                    start=mid+1;
-                }
+                if(nums[mid]<target && nums[end]>=target)start=mid+1;
                 else end=mid-1;
             }
         }
